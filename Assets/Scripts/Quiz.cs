108e12fbc1c10a7721b254d6df85d9ce5150c6f9
Edleron <edleron@hotmail.com>
2022-12-12 17:36:00 +0300

    fix : comp game

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,24 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField] QuestionSO question;
+    [Header("Questions")]
+    [SerializeField] private List<QuestionSO> questions = new List<QuestionSO>();
+    [SerializeField] private QuestionSO currentQuestion;
+    [SerializeField] private TextMeshProUGUI questionText;
 
-    [SerializeField] TextMeshProUGUI questionText;
+    [Header("Answer")]
+    [SerializeField] private GameObject[] answerButtons;
+    private int correctAnswerIndex;
+    private bool hasAnsweredEarly = true;
+    private string[] informationText = { "Birinci", "İkinci", "Üçüncü", "Dördüncü" };
 
-    [SerializeField] GameObject[] answerButtons;
+    [Header("Button Colors")]
+    [SerializeField] private Sprite defaultAnswerSprite;
+    [SerializeField] private Sprite correctAnswerSprite;
+    [SerializeField] private Sprite wrongAnswerSprite;
 
-    private void Start()
+    [Header("Timer")]
+    [SerializeField] private Image timerImage;
+    private Timer timer;
+
+    [Header("Scoring")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private ScoreKeeper scoreKeeper;
+
+    [Header("ProgressBar")]
+    [SerializeField] private Slider progressBar;
+    public bool isComplete;
+
+    private void Awake()
     {
-        questionText.text = question.GetQuestion();
+        timer = FindObjectOfType<Timer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
+    }
+
+    private void Update()
+    {
+        timerImage.fillAmount = timer.fiilFraction;
+        if (timer.loadNextQuestion)
+        {
+            if (progressBar.value == progressBar.maxValue)
+            {
+                isComplete = true;
+                return;
+            }
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnsweredEarly && !timer.isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        timer.CancalTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
+    }
+
+    public void DisplayAnswer(int index)
+    {
+        Image buttonImage;
+
+        if (index == currentQuestion.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Doğru !";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
+        }
+        else
+        {
+            correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+            string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
+            questionText.text = "Üzgünüm, Doğru cevap; \n" + informationText[correctAnswerIndex] + " şıktır.";
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = wrongAnswerSprite;
+        }
+    }
+
+    private void GetNextQuestion()
+    {
+        if (questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+            progressBar.value++;
+            scoreKeeper.IncrementQuestionsSeen();
+        }
+    }
+
+    private void GetRandomQuestion()
+    {
+        int index = Random.Range(0, questions.Count);
+        currentQuestion = questions[index];
+
+        if (questions.Contains(currentQuestion))
+        {
+            questions.Remove(currentQuestion);
+        }
+    }
+
+    private void DisplayQuestion()
+    {
+        questionText.text = currentQuestion.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
+            buttonText.text = currentQuestion.GetAnswer(i);
+        }
+    }
+
+    private void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    private void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
         }
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] private float timeToCompleteQuestion = 20f;
+    [SerializeField] private float timeToShowCorrectAnswer = 5f;
+
+    public bool loadNextQuestion = false;
+    public bool isAnsweringQuestion = false;
+    public float fiilFraction;
+    private float timerValue;
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void CancalTimer()
+    {
+        timerValue = 0;
+    }
+
+    private void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fiilFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fiilFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+    }
+}
